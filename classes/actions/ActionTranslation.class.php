0d9dbdcd721366bf40e615e21bc27c61bf5a9046
vatseek <vatseek13@rambler.ru>
2014-04-22 12:51:55 +0300

    #6796 add check for tran action

--- classes/actions/ActionTranslation.class.php
+++ classes/actions/ActionTranslation.class.php
@@ -17,6 +17,12 @@ class PluginL10n_ActionTranslation extends  ActionPlugin
 
     protected function EventTranslation()
     {
+        $oCurrentUser = $this->User_GetUserCurrent();
+        if (!$oCurrentUser || !$oCurrentUser->hasRole(Config::Get('plugin.l10n.role.translator'))) {
+            $this->Message_AddErrorSingle($this->Lang_Get('not_access'),$this->Lang_Get('error'));
+            return Router::Action('error');
+        }
+
         $iPage=$this->GetParamEventMatch(0,2) ? $this->GetParamEventMatch(0,2) : 1;
 
         $aFilter = array();
