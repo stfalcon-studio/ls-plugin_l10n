f72b4d6ecc15e04737da8f291f5bc6fa5e433694
vatseek <vatseek13@rambler.ru>
2014-04-25 17:06:08 +0300

    #translator extend ACL block post check function

--- PluginL10n.class.php
+++ PluginL10n.class.php
@@ -28,6 +28,7 @@ class PluginL10n extends Plugin
 			'ModuleTopic' => '_ModuleTopic',
 			'ModuleComment' => '_ModuleComment',
 			'ModuleUser' => '_ModuleUser',
+			'ModuleACL' => '_ModuleACL',
 			'PluginSitemap_ModuleSitemap' => 'PluginL10n_ModuleSitemap',
 		),
 		'entity' => array(
